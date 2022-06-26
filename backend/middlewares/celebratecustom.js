d2ba04584a3202edcda2d9429780d3491f3879af
Roman Kononov <doubleempty@yandex.ru>
2022-06-26 14:27:50 +0300

    15 work second del function

--- backend/middlewares/celebratecustom.js
+++ backend/middlewares/celebratecustom.js
@@ -3,13 +3,6 @@ const ErrorBadRequest = require('../errors/errorsbadrequest');
 
 const regExpURL = /^https?:\/\/(www\.)?[-\w:%\.\+~#=]{1,256}\.[a-z0-9()]{1,6}([-\w()@:%\.\+~#=//?&]*)/;
 
-const validateURL1 = (value) => {
-  if (!validator.isURL(value, { require_protocol: true })) {
-    throw new ErrorBadRequest('Неправильный формат ссылки');
-  }
-  return value;
-};
-
 const validateEmail = (value) => {
   if (!validator.isEmail(value, { require_protocol: true })) {
     throw new ErrorBadRequest('Неправильный формат email');
@@ -25,6 +18,5 @@ const validateURL = (value) => {
 
 module.exports = {
   validateURL,
-  validateURL1,
   validateEmail,
 };
